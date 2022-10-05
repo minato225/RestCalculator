0e2eaf7e0243a6495e2e3015be515f3f2e2b7558
minato225 <1815131bill@gmail.com>
2022-10-05 18:19:41 +0300

    Add project files.

---
+++ RestCalculator/Controllers/WeatherForecastController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestCalculator.Services;
+using System;
+
+namespace RestCalculator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        public ICalculator CalculatorService { get; }
+
+        public CalculatorController(ICalculator calculatorService) =>
+            CalculatorService = calculatorService;
+
+        [HttpGet("Add")]
+        public IActionResult Add(double a, double b)
+        {
+            var result = CalculatorService.Add(a, b);
+            return Ok(new
+            {
+                Result = result
+            });
+        }
+
+        [HttpGet("Div")]
+        public IActionResult Divide(double a, double b)
+        {
+            double result;
+
+            try
+            {
+                result = CalculatorService.Divide(a, b);
+            }
+            catch (DivideByZeroException e)
+            {
+                return BadRequest(new { message = e.Message });
+            }
+
+            return Ok(new
+            {
+                Result = result
+            });
+        }
+
+        [HttpGet("Sub")]
+        public IActionResult Subtract(double a, double b)
+        {
+            var result = CalculatorService.Subtract(a, b);
+            return Ok(new
+            {
+                Result = result
+            });
+        }
+
+        [HttpGet("Mul")]
+        public IActionResult Multiply(double a, double b)
+        {
+            var result = CalculatorService.Multiply(a, b);
+            return Ok(new
+            {
+                Result = result
+            });
+        }
+
+    }
+}
---
+++ RestCalculator/Services/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RestCalculator.Services
+{
+    public class Calculator : ICalculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("divisor should not be zero!");
+
+            return a / b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return b - a;
+        }
+    }
+}
